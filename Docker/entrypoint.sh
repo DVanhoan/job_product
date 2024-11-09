bd2470ea23a7a550ca8aa4e345c558eb7110fee4
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-10 01:39:54 +0700

    1

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,15 +4,14 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env ${APP_ENV}"
-    cp .env.example .env
-else
-    echo "Env file already exists"
-fi
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env ${APP_ENV}"
+#     cp .env.example .env
+# else
+#     echo "Env file already exists"
+# fi
 
 php artisan migrate
-php artisan key:generate
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
