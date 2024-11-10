7fb5a07c4fa055fd4f29b6a8ff6a1a6fbd9a7ad2
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-10 22:57:16 +0700

    dvh-commit

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,12 +4,12 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-# if [ ! -f ".env" ]; then
-#     echo "Creating env file for env ${APP_ENV}"
-#     cp .env.example .env
-# else
-#     echo "Env file already exists"
-# fi
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env ${APP_ENV}"
+    cp .env.example .env
+else
+    echo "Env file already exists"
+fi
 
 # until nc -z -v -w30 "${DB_HOST:-database}" 3306
 # do
