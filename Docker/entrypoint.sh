5a5b6bd1fc0a156d444afb65a6b50031af9935bb
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-10 23:39:24 +0700

    dvh-commit

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Echo các biến môi trường để kiểm tra
+echo "Environment variables:"
+echo "APP_ENV: $APP_ENV"
+echo "DB_CONNECTION: $DB_CONNECTION"
+echo "DB_HOST: $DB_HOST"
+echo "DB_DATABASE: $DB_DATABASE"
+echo "DB_USERNAME: $DB_USERNAME"
+echo "DB_PASSWORD: $DB_PASSWORD"
+echo "DB_PORT: $DB_PORT"
 
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
@@ -20,11 +29,16 @@ fi
 # echo "Database is up, proceeding with Laravel setup..."
 
 
+
 php artisan key:generate
+
+sleep 10
+
 php artisan migrate --force
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
+php artisan config:cache
 
 php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
 exec docker-php-entrypoint "$@"
