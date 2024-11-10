b1b25220967d60c96cafcc8879775c6e77c80f07
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-11 00:16:35 +0700

    dvh-commit

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,12 +4,12 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env locahost"
-    cp .env.example .env
-else
-    echo "Env file already exists"
-fi
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env locahost"
+#     cp .env.example .env
+# else
+#     echo "Env file already exists"
+# fi
 
 # until nc -z -v -w30 "${DB_HOST:-database}" 3306
 # do
@@ -22,7 +22,7 @@ fi
 
 
 
-php artisan key:generate
+# php artisan key:generate
 
 sleep 10
 
