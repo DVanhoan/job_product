5b57f4eb18edce072c73155e5903cc69f011fc03
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-10 22:53:15 +0700

    dvh-commit

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,20 +4,21 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env ${APP_ENV}"
-    cp .env.example .env
-else
-    echo "Env file already exists"
-fi
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env ${APP_ENV}"
+#     cp .env.example .env
+# else
+#     echo "Env file already exists"
+# fi
 
-until nc -z -v -w30 "${DB_HOST:-database}" 3306
-do
-  echo "Waiting for database connection..."
-  sleep 1
-done
+# until nc -z -v -w30 "${DB_HOST:-database}" 3306
+# do
+#   echo "Waiting for database connection..."
+#   sleep 1
+# done
 
-echo "Database is up, proceeding with Laravel setup..."
+# echo "Database is up, proceeding with Laravel setup..."
+
 
 php artisan key:generate
 php artisan migrate --force
