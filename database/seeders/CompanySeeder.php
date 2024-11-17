751fe00222bb5129106e5b48004a97edcdc45504
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-17 23:02:13 +0700

    dvh-commit

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -29,7 +29,7 @@ class CompanySeeder extends Seeder
             ],
             [
                 'user_id' => 4,
-                'company_category_id' => 1,
+                'company_category_id' => 3,
                 'logo' => 'https://res.cloudinary.com/dfac3tvue/image/upload/v1731221995/ar44lwoshfwdoqtom0uh.jpg',
                 'title' => 'LG Chem',
                 'description' => 'LG Chem là công ty con của tập đoàn LG, có trụ sở tại Hàn Quốc. Được thành lập vào năm 1947, LG Chem hiện là một trong những công ty hóa chất hàng đầu thế giới, với các lĩnh vực hoạt động chính bao gồm hóa chất cơ bản, hóa chất cao cấp, và vật liệu năng lượng. Công ty đóng vai trò tiên phong trong sản xuất pin Lithium-ion, cung cấp giải pháp năng lượng sạch cho các phương tiện điện như xe ô tô và các thiết bị công nghiệp khác. Không chỉ dừng lại ở lĩnh vực năng lượng, LG Chem còn sản xuất các loại nhựa tiên tiến, vật liệu công nghệ sinh học, và giải pháp công nghệ môi trường, hướng tới mục tiêu phát triển bền vững và thân thiện với môi trường. Công ty cam kết tạo ra giá trị vượt trội cho khách hàng thông qua các sản phẩm chất lượng cao và đổi mới liên tục.',
@@ -38,7 +38,7 @@ class CompanySeeder extends Seeder
             ],
             [
                 'user_id' => 7,
-                'company_category_id' => 1,
+                'company_category_id' => 4,
                 'logo' => 'https://res.cloudinary.com/dfac3tvue/image/upload/v1731078014/cdvujn8u7ij5dhpcnseh.jpg',
                 'title' => 'VINFAST',
                 'description' => 'VinFast, thuộc tập đoàn Vingroup, là nhà sản xuất phương tiện chạy điện thông minh hàng đầu Việt Nam. Được thành lập vào năm 2017, VinFast đặt mục tiêu trở thành thương hiệu xe điện toàn cầu, với tầm nhìn thúc đẩy cuộc cách mạng giao thông xanh và bền vững. Sản phẩm của VinFast bao gồm xe máy điện, ô tô điện, và xe buýt điện, được phát triển dựa trên các tiêu chuẩn chất lượng cao của châu Âu và Mỹ. Không chỉ tập trung vào sản phẩm, VinFast còn xây dựng mạng lưới trạm sạc điện rộng khắp, hỗ trợ khách hàng chuyển đổi sang sử dụng năng lượng sạch một cách dễ dàng và tiện lợi. Với đội ngũ chuyên gia kỹ thuật hàng đầu và quan hệ đối tác chiến lược với các công ty công nghệ lớn, VinFast đang từng bước khẳng định vị thế trên thị trường quốc tế.',
@@ -47,7 +47,7 @@ class CompanySeeder extends Seeder
             ],
             [
                 'user_id' => 8,
-                'company_category_id' => 1,
+                'company_category_id' => 6,
                 'logo' => 'https://res.cloudinary.com/dfac3tvue/image/upload/v1731150617/dbt0kestyjodfhcrcmrp.jpg',
                 'title' => 'Google',
                 'description' => 'Google, một trong những công ty công nghệ tiên phong trên thế giới, được thành lập vào năm 1998 bởi Larry Page và Sergey Brin. Ban đầu, Google chỉ là một công cụ tìm kiếm, nhưng hiện tại, công ty đã mở rộng thành một hệ sinh thái công nghệ khổng lồ với hàng loạt sản phẩm và dịch vụ đột phá. Google nổi tiếng với các sản phẩm như Google Search, Gmail, Google Maps, YouTube, và hệ điều hành Android, phục vụ hàng tỷ người dùng trên toàn cầu. Ngoài ra, Google còn đóng vai trò dẫn đầu trong lĩnh vực trí tuệ nhân tạo, máy học, và các dịch vụ điện toán đám mây với Google Cloud. Sứ mệnh của Google là "tổ chức thông tin của thế giới, để nó trở nên dễ dàng truy cập và hữu ích hơn cho tất cả mọi người." Với cam kết không ngừng đổi mới, Google đang tiếp tục thúc đẩy sự tiến bộ công nghệ và đem lại giá trị bền vững cho cộng đồng.',
@@ -56,7 +56,7 @@ class CompanySeeder extends Seeder
             ],
             [
                 'user_id' => 9,
-                'company_category_id' => 1,
+                'company_category_id' => 2,
                 'logo' => 'https://res.cloudinary.com/dfac3tvue/image/upload/v1731220266/ulxgbiesmdtegixn8zbx.jpg',
                 'title' => 'Apple',
                 'description' => 'Apple Inc. là một trong những tập đoàn công nghệ sáng tạo nhất trên thế giới, có trụ sở chính tại Cupertino, California, Hoa Kỳ. Được thành lập vào năm 1976 bởi Steve Jobs, Steve Wozniak, và Ronald Wayne, Apple đã làm thay đổi cách con người sử dụng công nghệ với các sản phẩm biểu tượng như iPhone, iPad, MacBook, Apple Watch và AirPods. Triết lý thiết kế tối giản, tinh tế và tập trung vào trải nghiệm người dùng là yếu tố giúp Apple luôn dẫn đầu ngành công nghiệp công nghệ. Bên cạnh các sản phẩm phần cứng, Apple cũng cung cấp dịch vụ phần mềm như iOS, macOS, và các dịch vụ trực tuyến như Apple Music, iCloud, và App Store. Với cam kết bảo vệ quyền riêng tư và môi trường, Apple đang hướng tới việc sử dụng 100% năng lượng tái tạo trong toàn bộ chuỗi cung ứng.',
@@ -65,7 +65,7 @@ class CompanySeeder extends Seeder
             ],
             [
                 'user_id' => 10,
-                'company_category_id' => 1,
+                'company_category_id' => 4,
                 'logo' => 'https://res.cloudinary.com/dfac3tvue/image/upload/v1731222301/rlbgnmdpoikejdvm3hux.png',
                 'title' => 'Kiaisoft',
                 'description' => 'Kiaisoft là một công ty khởi nghiệp công nghệ trẻ trung và năng động, được thành lập vào năm 2019 với trụ sở tại Việt Nam. Công ty tập trung phát triển các sản phẩm phần mềm và dịch vụ công nghệ dành cho thị trường Nhật Bản, một trong những thị trường đòi hỏi chất lượng và sự chính xác cao. Kiaisoft chuyên cung cấp các giải pháp phần mềm tùy chỉnh, bao gồm phát triển ứng dụng di động, web, và tự động hóa quy trình kinh doanh (RPA). Với đội ngũ nhân sự sáng tạo và kinh nghiệm, Kiaisoft không chỉ đáp ứng yêu cầu của khách hàng mà còn đề xuất các giải pháp đột phá giúp tối ưu hóa hiệu suất hoạt động. Trong những năm tới, Kiaisoft hướng đến việc mở rộng sang các thị trường quốc tế khác và trở thành biểu tượng của sự đổi mới trong ngành công nghệ Việt Nam.',
@@ -89,13 +89,6 @@ class CompanySeeder extends Seeder
             // Tạo các công việc cho mỗi công ty
             $this->createPostsForCompany($companyRecord);
         };
-
-        DB::table('post_user')->insert([
-            [
-                'post_id' => 4,
-                'user_id' => 11
-            ]
-        ]);
     }
 
 
