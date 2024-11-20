7d3017894c7ab7ced803c82b860ec269bdc162b2
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-21 04:00:58 +0700

    dvh-commit

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,38 +1,34 @@
 <section class="bg-dark py-5">
-    <footer class="container mx-auto row px-5">
+    <footer class="container mx-auto d-flex flex-wrap justify-content-between align-items-start px-5">
 
-        <div class="col-12 col-md-4 mb-4 text-center text-md-start">
-            <img
-                src="{{ asset('Logo.svg') }}"
-                alt="logo"
-                class="invert img-fluid mx-auto d-block d-md-inline-block"
-            />
-            <p class="text-muted mt-3">
+        <div class="col-12 col-md-3 text-center text-md-start mb-4">
+            <img src="{{ asset('Logo.svg') }}" alt="logo" class="invert img-fluid mb-3" />
+            <p class="text-muted">
                 Ehya is a job board and job site for job seekers and employers.
             </p>
             <ul class="list-inline mt-4">
                 <li class="list-inline-item">
-                    <a href="/" class="text-secondary">
+                    <a href="https://twitter.com/" target="_blank" class="text-secondary">
                         <i class="fab fa-twitter fa-lg"></i>
                     </a>
                 </li>
                 <li class="list-inline-item">
-                    <a href="/" class="text-secondary">
+                    <a href="https://www.youtube.com/" target="_blank" class="text-secondary">
                         <i class="fab fa-youtube fa-lg"></i>
                     </a>
                 </li>
                 <li class="list-inline-item">
-                    <a href="/" class="text-secondary">
+                    <a href="https://www.instagram.com/" target="_blank" class="text-secondary">
                         <i class="fab fa-instagram fa-lg"></i>
                     </a>
                 </li>
                 <li class="list-inline-item">
-                    <a href="/" class="text-secondary">
+                    <a href="https://www.facebook.com/" target="_blank" class="text-secondary">
                         <i class="fab fa-facebook fa-lg"></i>
                     </a>
                 </li>
                 <li class="list-inline-item">
-                    <a href="/" class="text-secondary">
+                    <a href="https://t.me/" target="_blank" class="text-secondary">
                         <i class="fab fa-telegram fa-lg"></i>
                     </a>
                 </li>
@@ -40,7 +36,7 @@
         </div>
 
 
-        <div class="col-6 col-md-2 mb-4">
+        <div class="col-6 col-md-2 text-center text-md-start mb-4">
             <h5 class="text-light">Search for jobs</h5>
             <ul class="list-unstyled mt-3">
                 <li><a href="{{ route('job.index') }}" class="text-muted">Job by location</a></li>
@@ -52,7 +48,7 @@
         </div>
 
 
-        <div class="col-6 col-md-2 mb-4">
+        <div class="col-6 col-md-2 text-center text-md-start mb-4">
             <h5 class="text-light">For employers</h5>
             <ul class="list-unstyled mt-3">
                 <li><a href="{{ route('post.create') }}" class="text-muted">Create job</a></li>
@@ -64,7 +60,7 @@
         </div>
 
 
-        <div class="col-6 col-md-2 mb-4">
+        <div class="col-6 col-md-2 text-center text-md-start mb-4">
             <h5 class="text-light">About</h5>
             <ul class="list-unstyled mt-3">
                 <li><a href="/" class="text-muted">About us</a></li>
@@ -73,20 +69,76 @@
                 <li><a href="/" class="text-muted">Contact</a></li>
             </ul>
         </div>
-
-
-        <div class="col-12 text-center mt-5">
-            <div class="bg-primary text-white p-3 rounded-circle d-inline-block">
-                <i class="fas fa-heart fa-lg"></i>
-            </div>
-            <p class="mt-3 font-weight-bold text-muted">
-                Copyright © 2023. All rights reserved
-            </p>
-        </div>
     </footer>
+
+
 </section>
 
 
 <button onclick="scrollToTop()" id="scrollToTopBtn" class="scroll-to-top">
     <i class="fas fa-arrow-up"></i>
 </button>
+
+
+<style>
+    footer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+}
+
+footer .col-md-3 img {
+    max-width: 150px;
+}
+
+footer h5 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+}
+
+footer ul {
+    padding-left: 0;
+    list-style: none;
+}
+
+footer ul li {
+    margin-bottom: 0.5rem;
+}
+
+footer ul li a {
+    color: #6c757d;
+    transition: color 0.3s;
+}
+
+footer ul li a:hover {
+    color: #fff;
+}
+
+footer .list-inline-item a {
+    transition: color 0.3s;
+}
+
+footer .list-inline-item a:hover {
+    color: #fff;
+}
+
+.scroll-to-top {
+    position: fixed;
+    bottom: 2rem;
+    right: 2rem;
+    background: #08036e;
+    color: #fff;
+    border: none;
+    border-radius: 50%;
+    padding: 0.75rem 1rem;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: background 0.3s;
+}
+
+.scroll-to-top:hover {
+    background: #574bff;
+}
+
+</style>
