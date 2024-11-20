81b63a107321e8ca068b32557b69776b7aa214ba
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-11-21 02:32:45 +0700

    dvh-commit

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,5 @@
 <section class="bg-dark py-5">
     <footer class="container mx-auto row px-5">
-        {{-- Logo và giới thiệu --}}
         <div class="col-12 col-md-4 mb-4 text-center text-md-start">
             <img
                 src="{{ asset('Logo.svg') }}"
@@ -8,7 +7,7 @@
                 class="invert img-fluid mx-auto d-block d-md-inline-block"
             />
             <p class="text-muted mt-3">
-                Build a modern and creative website with crealand
+                Ehya is a job board and job site for job seekers and employers.
             </p>
             <ul class="list-inline mt-4">
                 <li class="list-inline-item">
@@ -39,52 +38,42 @@
             </ul>
         </div>
 
-        {{-- Cột Product --}}
+
         <div class="col-6 col-md-2 mb-4">
-            <h5 class="text-light">Product</h5>
+            <h5 class="text-light">Search for jobs</h5>
             <ul class="list-unstyled mt-3">
-                <li><a href="/" class="text-muted">Landingpage</a></li>
-                <li><a href="/" class="text-muted">Features</a></li>
-                <li><a href="/" class="text-muted">Documentation</a></li>
-                <li><a href="/" class="text-muted">Referral Program</a></li>
-                <li><a href="/" class="text-muted">Pricing</a></li>
+                <li><a href="{{ route('job.index') }}" class="text-muted">Job by location</a></li>
+                <li><a href="{{ route('job.index') }}" class="text-muted">Job by category</a></li>
+                <li><a href="{{ route('job.index') }}" class="text-muted">Job by company</a></li>
+                <li><a href="{{ route('job.index') }}" class="text-muted">Job by keyword</a></li>
+                <li><a href="{{ route('job.index') }}" class="text-muted">Job by salary</a></li>
             </ul>
         </div>
 
-        {{-- Cột Services --}}
+
         <div class="col-6 col-md-2 mb-4">
-            <h5 class="text-light">Services</h5>
+            <h5 class="text-light">For employers</h5>
             <ul class="list-unstyled mt-3">
-                <li><a href="/" class="text-muted">Documentation</a></li>
-                <li><a href="/" class="text-muted">Design</a></li>
-                <li><a href="/" class="text-muted">Themes</a></li>
-                <li><a href="/" class="text-muted">Illustrations</a></li>
-                <li><a href="/" class="text-muted">UI Kit</a></li>
+                <li><a href="{{ route('post.create') }}" class="text-muted">Create job</a></li>
+                <li><a href="{{ route('account.authorSection') }}" class="text-muted">Manage job</a></li>
+                <li><a href="{{ route('jobApplication.index') }}" class="text-muted">Job applications</a></li>
+                <li><a href="{{ route('savedJob.index') }}" class="text-muted">Manage candidates</a></li>
+                <li><a href="{{ route('company.store') }}" class="text-muted">Manage company</a></li>
             </ul>
         </div>
 
-        {{-- Cột Company --}}
+
         <div class="col-6 col-md-2 mb-4">
-            <h5 class="text-light">Company</h5>
+            <h5 class="text-light">About</h5>
             <ul class="list-unstyled mt-3">
-                <li><a href="/" class="text-muted">About</a></li>
-                <li><a href="/" class="text-muted">Terms</a></li>
-                <li><a href="/" class="text-muted">Privacy Policy</a></li>
-                <li><a href="/" class="text-muted">Careers</a></li>
+                <li><a href="/" class="text-muted">About us</a></li>
+                <li><a href="/" class="text-muted">What we do</a></li>
+                <li><a href="/" class="text-muted">Our team</a></li>
+                <li><a href="/" class="text-muted">Contact</a></li>
             </ul>
         </div>
 
-        {{-- Cột More --}}
-        <div class="col-6 col-md-2 mb-4">
-            <h5 class="text-light">More</h5>
-            <ul class="list-unstyled mt-3">
-                <li><a href="/" class="text-muted">Documentation</a></li>
-                <li><a href="/" class="text-muted">License</a></li>
-                <li><a href="/" class="text-muted">Changelog</a></li>
-            </ul>
-        </div>
 
-        {{-- Dòng bản quyền --}}
         <div class="col-12 text-center mt-5">
             <div
                 class="bg-primary text-white p-3 rounded-circle d-inline-block"
@@ -92,14 +81,13 @@
                 <i class="fas fa-heart fa-lg"></i>
             </div>
             <p class="mt-3 font-weight-bold text-muted">
-                Copyright © 2023. Crafted with love.
+                Copyright © 2023. All rights reserved
             </p>
         </div>
     </footer>
 </section>
 
 
-<!-- Nút cuộn lên đầu trang -->
 <button onclick="scrollToTop()" id="scrollToTopBtn" class="scroll-to-top">
     <i class="fas fa-arrow-up"></i>
 </button>
