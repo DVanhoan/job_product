f871093aaaf379f92222a2b56b1a060c893dc9a1
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2024-12-10 17:07:22 +0700

    update provins

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,51 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\CompanyCategory;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use App\http\Controllers\ProvinceController;
+use App\Services\ProvinceService;
 
 class JobController extends Controller
 {
-    private $provinceController;
-
-    public function __construct(ProvinceController $provinceController)
+    private $provinceService;
+    public function __construct(ProvinceService $provinceService)
     {
-        $this->provinceController = $provinceController;
+        $this->provinceService = $provinceService;
     }
-
     public function index(Request $request)
     {
         $categories = CompanyCategory::all();
-
-        $provinces = $this->provinceController->getProvinces();
-        // dd($provinces);
-
-        $posts = Post::query();
-        if ($request->q) {
-            $posts = $posts->where('job_title', 'LIKE', '%' . $request->q . '%')->orWhere('skills', 'LIKE', '%' . $request->q . '%');
-        }
-        if ($request->category_id) {
-            $posts = $posts->whereHas('company', function ($query) use ($request) {
-                $query->where('company_category_id', $request->category_id);
-            });
-        }
-        if ($request->job_level) {
-            $posts = $posts->where('job_level', 'LIKE', '%' . $request->job_level . '%');
-        }
-        if ($request->education_level) {
-            $posts = $posts->where('education_level', 'LIKE', '%' . $request->education_level . '%');
-        }
-        if ($request->employment_type) {
-            $posts = $posts->where('employment_type', 'LIKE', '%' . $request->employment_type . '%');
-        }
-        if ($request->job_location) {
-            $posts = $posts->where('job_location', 'LIKE', '%' . $request->job_location . '%');
-        }
-
-        $posts = $posts->has('company')->with('company')->orderBy('views', 'desc')->paginate(6);
-
+        $provinces = $this->getProvinces();
+        $posts = Post::query()
+            ->with('company')
+            ->when($request->q, function ($query, $q) {
+                return $query->where(function ($qBuilder) use ($q) {
+                    $qBuilder->where('job_title', 'LIKE', "%$q%")
+                        ->orWhere('skills', 'LIKE', "%$q%");
+                });
+            })
+            ->when($request->category_id, function ($query, $categoryId) {
+                return $query->whereHas('company', function ($companyQuery) use ($categoryId) {
+                    $companyQuery->where('company_category_id', $categoryId);
+                });
+            })
+            ->when($request->job_level, fn($query, $jobLevel) => $query->where('job_level', 'LIKE', "%$jobLevel%"))
+            ->when($request->education_level, fn($query, $eduLevel) => $query->where('education_level', 'LIKE', "%$eduLevel%"))
+            ->when($request->employment_type, fn($query, $empType) => $query->where('employment_type', 'LIKE', "%$empType%"))
+            ->when($request->job_location, fn($query, $location) => $query->where('job_location', 'LIKE', "%$location%"))
+            ->has('company')
+            ->orderBy('views', 'desc')
+            ->paginate(6);
         return view('job.index', compact('posts', 'categories', 'provinces'));
+    }
+    public function getProvinces()
+    {
+        $dataObject = $this->provinceService->getProvinces();
+        if (!$dataObject || empty($dataObject['results'])) {
+            return [];
+        }
+        $provinces = collect($dataObject['results'])->map(function ($item) {
+            return (object) [
+                'id' => $item['province_id'],
+                'name' => $item['province_name'],
+                'type' => $item['province_type']
+            ];
+        })->all();
+        return $provinces;
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,16 +8,15 @@ use App\Models\CompanyCategory;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
-use App\Http\Controllers\ProvinceController;;
+use App\Services\ProvinceService;
 
 class PostController extends Controller
 {
-    private $provinceController;
-    public function __construct(ProvinceController $provinceController)
+    private $provinceService;
+    public function __construct(ProvinceService $provinceService)
     {
-        $this->provinceController = $provinceController;
+        $this->provinceService = $provinceService;
     }
-
     public function index()
     {
         $posts = Post::has('company')->with('company')->orderBy('views', 'desc')->paginate(12);
@@ -29,44 +28,44 @@ class PostController extends Controller
             'topEmployers' => $topEmployers
         ]);
     }
-
-
+    public function getProvinces()
+    {
+        $dataObject = $this->provinceService->getProvinces();
+        return array_map(function ($data) {
+            return (object) [
+                'id' => $data['province_id'],
+                'name' => $data['province_name'],
+                'type' => $data['province_type']
+            ];
+        }, $dataObject['results']);
+    }
     public function create()
     {
-        $provinces = $this->provinceController->getProvinces();
+        $provinces = $this->getProvinces();
         if (!auth()->user()->company) {
             Alert::info('You must create a company first!', 'info');
             return redirect()->route('company.create');
         }
         return view('post.create', compact('provinces'));
     }
-
     public function store(Request $request)
     {
         $this->requestValidate($request);
-
         $postData = $request->all();
         $postData['company_id'] = auth()->user()->company->id;
-
         $post = Post::create($postData);
-
         if ($post) {
             Alert::success('Created Post!', 'success');
             return redirect()->route('account.authorSection');
         }
-
         Alert::warning('Post failed to list!', 'warning');
         return redirect()->back();
     }
-
-
     public function show($id)
     {
         $post = Post::findOrFail($id);
-
         event(new PostViewEvent($post));
         $company = $post->company()->first();
-
         $similarPosts = Post::whereHas('company', function ($query) use ($company) {
             return $query->where('company_category_id', $company->company_category_id);
         })->where('id', '<>', $post->id)->with('company')->take(5)->get();
@@ -76,30 +75,22 @@ class PostController extends Controller
             'similarJobs' => $similarPosts
         ]);
     }
-
-
     public function edit(Post $post)
     {
-        $provinces = $this->provinceController->getProvinces();
+        $provinces = $this->getProvinces();
         return view('post.edit', compact('post', 'provinces'));
     }
-
     public function update(Request $request, $post)
     {
         $this->requestValidate($request);
-
         $getPost = Post::findOrFail($post);
-
         if ($getPost->update($request->all())) {
             Alert::success('Post successfully updated!', 'success');
             return redirect()->route('account.authorSection');
         }
-
         Alert::error('Failed to update post!', 'error');
         return redirect()->route('post.index');
     }
-
-
     public function destroy(Post $post)
     {
         if ($post->delete()) {
@@ -108,7 +99,6 @@ class PostController extends Controller
         }
         return redirect()->back();
     }
-
     protected function requestValidate($request)
     {
         return $request->validate([
