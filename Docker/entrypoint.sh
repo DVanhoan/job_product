0b589017456743197ed6a694f7fd6236f95811b2
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2025-03-17 16:27:37 +0700

    fd

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -12,8 +12,8 @@ else
 fi
 
 php artisan key:generate
-php artisan migrate --force
-php artisan db:seed --force
+# php artisan migrate --force
+# php artisan db:seed --force
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
